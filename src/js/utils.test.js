81256d17b81ed13020c51aa38ec01cb5b970a8dd
e.kirsanov <e.kirsanov@netris.ru>
2019-02-08 18:31:18 +0300

    prettierrc

--- src/js/utils.test.js
+++ src/js/utils.test.js
@@ -1,5 +1,16 @@
-import sum from "./utils";
+import { sum } from './utils';
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+/**
+ * expect and toBe - для проверки идентичности данных
+ *
+ */
+test('Ф-ия сложения проверяем 1 + 2 = 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('Проверяем равенство объекта data1 и data2', () => {
+    const data1 = { один: 1 };
+    const data2 = { один: 1, два: 2 };
+    data1['два'] = 2;
+    expect(data1).toEqual(data2);
 });
