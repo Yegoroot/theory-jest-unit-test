cb0ce4db2560580545e8bd0bb02f06dc1f83f272
e.kirsanov <e.kirsanov@netris.ru>
2019-02-08 20:26:25 +0300

    config dev serv

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,17 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'my-first-webpack.bundle.js'
-    },
-
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
+        filename: 'bundle.js'
     }
+
+    // devServer: {
+    //     contentBase: path.join(__dirname, 'dist'),
+    //     compress: true,
+    //     port: 9000
+    // }
 };
